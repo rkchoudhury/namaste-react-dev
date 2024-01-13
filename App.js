b61c965bb476fea01c4669d7787fcbe346d654c3
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2024-01-13 15:31:28 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement(
+    "h1",
+    { id: "heading", xyz: "abc" }, 
+    "Hello World from React!"
+);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(root);
+
+root.render(heading);
