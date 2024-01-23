e2d14d088342455c172de2d361cf39ba56e0593d
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2024-01-23 20:05:57 +0530

    episode-03: Added Header and Body and Restaurant components

--- App.js
+++ App.js
@@ -1,16 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-    "h1",
-    { id: "heading", xyz: "abc" }, 
-    "Hello World from React!"
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/food-app-icon-food-explorer-design-template-8ae900e41ccbc0a2e1b48a85d239e389_screen.jpg?ts=1585237320"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>
+            <img
+              className="cart-icon"
+              src="https://png.pngtree.com/png-vector/20190114/ourmid/pngtree-vector-shopping-cart-icon-png-image_313581.jpg"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-console.log(heading);
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/x4uyxvihmg8qa3pddkgf"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biriyani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 mins</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <diV className="res-container">
+        <RestaurantCard />
+      </diV>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 console.log(root);
 
-root.render(heading);
+root.render(<AppLayout />);
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Namaste React
+
+# Commands
+
+npm install parcel
+npm install react
+npm install react-dom
+
+# To start the bundler
+npm parcel index.js
