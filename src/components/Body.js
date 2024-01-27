2cf863956b3d1d567f748b4711fbb213d3a1c790
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2024-01-27 18:58:43 +0530

    episode-05: Added filter button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
 import RestaurantCard from "./RestaurantCard";
 import restaurants from "../utils/mockRestaurantData";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRestaurants = listOfRestaurants.filter(
+              (restaurant) => restaurant.info.avgRating >= 4.5
+            );
+            setListOfRestaurants(filteredRestaurants);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {restaurants.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
