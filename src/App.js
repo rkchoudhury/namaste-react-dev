45a26bbb013bc038e0fe0bf8cfc77883f39030f4
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2024-01-30 19:36:45 +0530

    episode-07: Fixed the header and render the about and contact screen contents

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -12,12 +12,16 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      {/* <Body /> */}
+      <Outlet />
     </div>
   );
 };
 
-const rootRouter = createBrowserRouter([
+/**
+ * Here with each links we are navigating to different pages
+ */
+const rootRouter1 = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
@@ -30,6 +34,32 @@ const rootRouter = createBrowserRouter([
   {
     path: "/contact",
     element: <Contact />,
+  },
+]);
+
+/**
+ * By making the children of AppLayout component we are rendering About and Contact screen on the same screen
+ * Here the Header will be fixed and the Outlet content will be replaced by the specified element when we are navigating to the specified path
+ */
+const rootRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
